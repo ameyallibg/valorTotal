24fd1f9646fd49cdf86668c28c32ba4c28663c58
unknown <bgameyalli@gmail.com>
2019-10-30 13:57:08 -0600

    Subiendo arreglo en nav y editar y eliminar

--- src/components/ListaOrdenes/index.js
+++ src/components/ListaOrdenes/index.js
@@ -17,8 +17,9 @@ OrdenCreada.contextType = AppContext;
 
 class  ListaOrdenes extends React.Component {
   render () {
-    const{items, onClickItem, handleChangeFound , handleChangeDate, handleChangeSelect, deleteFilter, handleChangeSeller, handleChangeProject}=this.context
-  return (
+    const{items, onClickItem, rol, handleUpdate, getName, handleChangeFound , onDelete,  handleChangeDate, handleChangeSelect, deleteFilter, handleChangeSeller, handleChangeProject}=this.context
+    if(rol === "admin") {
+      return (
       <div>
          
          
@@ -126,13 +127,13 @@ class  ListaOrdenes extends React.Component {
      
         <tr key={index} className="list">
           <td></td>
-          <td className="text-center"><EditOutlinedIcon/><FindInPageOutlinedIcon  id={item.productClave} onClick={onClickItem}/></td>
+          <td className="text-center"><EditOutlinedIcon id={item.productClave} onClick={onClickItem} /><FindInPageOutlinedIcon  id={item.productClave} onClick={onClickItem}/></td>
           <td>{item.productClave}</td>
           <td>{item.vendedor}</td>
           <td>{item.uge}</td>
           <td>{item.date}</td>
           <td>{item.estatus}</td>
-          <td><DeleteOutlineOutlinedIcon /></td>
+          <td><DeleteOutlineOutlinedIcon id={item.productClave} onClick={onDelete}/></td>
         </tr>
         
         
@@ -146,6 +147,132 @@ class  ListaOrdenes extends React.Component {
   
     </div>
   );
+}else {
+  return (
+    <div>
+       
+       
+              
+           
+
+
+                <Link  className="text-white " to="/OrdenTrabajo"><Button className='mt-2 mb-2 mr-3 float-right' color="success ">Crear</Button></Link>
+            
+                
+                
+                <Table>
+                <thead>
+              </thead>
+              <tbody>
+        <tr  className="filtrar-style">
+          <th></th>
+          <th>Filtrar por</th>
+        <th>
+          <Input type="text" name="buscador" placeholder="Clave" onChange={handleChangeFound} >
+          </Input>
+          
+        </th>
+        <th>
+          <Input type="text" name="nombreVendedor" readOnly value={getName} >
+            
+          
+              
+          </Input>
+        </th>
+        <th>
+           <Input type="select" name="tipoProyecto" onChange={handleChangeProject}>
+              <option value="">Proyecto</option>
+             
+     
+              <option value="CIVIL">CIVIL</option>
+                       <option value="FINANCIERA">FINANCIERA</option>
+                       <option value="INDUSTRIAL">INDUSTRIAL</option>
+                       <option value="CIVIL-INDUSTRIAL">CIVIL-INDUSTRIAL</option>
+                       <option value="CIVIL-FINANCIERA">CIVIL-FINANCIERA</option>
+                       <option value="FINANCIERA-INDUSTRIAL">FINANCIERA-INDUSTRIAL</option>
+                       <option value="CIVIL-FINANCIERA-INDUSTRIAL">CIVIL-FINANCIERA-INDUSTRIAL</option>
+          </Input>
+        </th>
+        <th>
+        <Input type="date" name="fechaBuscador" onChange={handleChangeDate} >
+          Fecha
+          </Input>
+        </th>
+        <th>
+           <Input type="select" name="estatus" onChange={handleChangeSelect}>
+              <option value="" >Estatus</option>
+              <option value="vendido">Vendido</option>
+              <option value="proceso">En proceso</option>
+              <option value="cancelado">Cancelado</option>
+          </Input>
+        </th>
+        <th><Button   color="warning text-white" onClick={deleteFilter}><RotateLeftIcon/> </Button></th>
+      </tr>
+      <tr><th></th></tr>
+      
+     
+        
+     
+      
+      </tbody>
+
+      
+       
+
+      
+             
+          
+          
+         
+      
+
+    <thead>
+     
+      <tr  className="tabla-style">
+      
+
+        <th></th>
+        <th className="text-center"><FolderOpenIcon/></th>
+        <th >Clave</th>
+        <th>Vendedor</th>
+        <th>Proyecto</th>
+        <th>Fecha</th>
+        <th>Estatus</th>
+        <th></th>
+        
+      </tr>
+     
+    </thead>
+    
+    <tbody>
+    {items.map((item, index) =>(
+          
+          
+   
+      <tr key={index} className="list">
+        <td></td>
+        <td className="text-center"><EditOutlinedIcon/><FindInPageOutlinedIcon  id={item.productClave} onClick={onClickItem}/></td>
+        <td>{item.productClave}</td>
+        <td>{item.vendedor}</td>
+        <td>{item.uge}</td>
+        <td>{item.date}</td>
+        <td>{item.estatus}</td>
+        <td></td>
+      </tr>
+      
+      
+   
+      ))
+              }
+    </tbody>
+   
+    </Table>
+  <OrdenCreada/>
+
+  </div>
+);
+
+}
 }
 }
 
