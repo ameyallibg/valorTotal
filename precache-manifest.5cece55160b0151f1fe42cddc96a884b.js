88c5e1614f5f92985d69b80842de553b2e2b7d35
bgameyalli <bgameyalli@gmail.com>
2021-01-08 16:00:47 -0600

    Updates

---
+++ precache-manifest.5cece55160b0151f1fe42cddc96a884b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5394f2dbec06e4dc4cab4d83fe44e589",
+    "url": "/valorTotal/index.html"
+  },
+  {
+    "revision": "9edd6e0512026fd6e6ca",
+    "url": "/valorTotal/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "241c7071fda3e8f12db8",
+    "url": "/valorTotal/static/css/main.3e6b134c.chunk.css"
+  },
+  {
+    "revision": "9edd6e0512026fd6e6ca",
+    "url": "/valorTotal/static/js/2.8a76394f.chunk.js"
+  },
+  {
+    "revision": "241c7071fda3e8f12db8",
+    "url": "/valorTotal/static/js/main.bf054a08.chunk.js"
+  },
+  {
+    "revision": "58429bb57dd7dd3bbd5e",
+    "url": "/valorTotal/static/js/runtime-main.023578e8.js"
+  },
+  {
+    "revision": "0f42d4925c76f08392741c3fa0fb73cd",
+    "url": "/valorTotal/static/media/cliente.0f42d492.png"
+  },
+  {
+    "revision": "ca13ec1cba2299f31e6f385c12c08dae",
+    "url": "/valorTotal/static/media/estadisticas.ca13ec1c.png"
+  },
+  {
+    "revision": "fbb02c6c83928c721e0fb0af11dc81ed",
+    "url": "/valorTotal/static/media/negocios.fbb02c6c.jpg"
+  }
+]);
